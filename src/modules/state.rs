fc1a2492d78f6e932ced7ef0154ea71a42eb8a75
Sufhal <jules.girold@gmail.com>
2024-07-18 00:06:56 +0200

    chore: cleaning things up

--- src/modules/state.rs
+++ src/modules/state.rs
@@ -273,35 +273,6 @@ impl<'a> State<'a> {
                 .await
                 .unwrap();
 
-        const SPACE_BETWEEN: f32 = 3.0;
-        let instances = (0..NUM_INSTANCES_PER_ROW).flat_map(|z| {
-            (0..NUM_INSTANCES_PER_ROW).map(move |x| {
-                let x = SPACE_BETWEEN * (x as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-                let z = SPACE_BETWEEN * (z as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-
-                let position = cgmath::Vector3 { x, y: 0.0, z };
-
-                let rotation = if position.is_zero() {
-                    cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0))
-                } else {
-                    cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(45.0))
-                };
-
-                instance::Instance {
-                    position, rotation,
-                }
-            })
-        }).collect::<Vec<_>>();
-
-        // let instance_data = instances.iter().map(instance::Instance::to_raw).collect::<Vec<_>>();
-        // let instance_buffer = device.create_buffer_init(
-        //     &wgpu::util::BufferInitDescriptor {
-        //         label: Some("Instance Buffer"),
-        //         contents: bytemuck::cast_slice(&instance_data),
-        //         usage: wgpu::BufferUsages::VERTEX,
-        //     }
-        // );
-
         let mut scene = scene::Scene::new();
         let mut object_3d = Object3D::new(&device, Some(obj_model));
         for i in 1..10 {
